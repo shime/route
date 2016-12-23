8023efdfca27f3497b4f44ef240607d4eb8a7071
Tsutomu Kawamura <cognitom@gmail.com>
2016-12-23 13:18:44 +0900

    Creates a new package `riot-route-tag`

---
+++ packages/riot-route-tag/README.md
@@ -0,0 +1,25 @@
+# riot-route-tag
+
+> Tag-based router for Riot.js
+
+riot-route-tag is built on [riot-route](https://github.com/riot/route) and [Riot.js](https://github.com/riot/riot).
+
+```html
+<app>
+  <router>
+    <route path="apple"><p>Apple</p></route>
+    <route path="banana"><p>Banana</p></route>
+    <route path="coffee"><p>Coffee</p></route>
+  </router>
+</app>
+```
+
+## Install
+
+```bash
+$ npm install --save riot-route
+```
+
+## License
+
+MIT (c) Muut, Inc. and other contributors
--- rollup.js
+++ rollup.js
@@ -1,7 +1,8 @@
 const
   rollup = require('rollup'),
   buble = require('rollup-plugin-buble'),
-  alias = require('rollup-plugin-alias')
+  alias = require('rollup-plugin-alias'),
+  riot = require('rollup-plugin-riot')
 
 const
   packageRiotRoute = 'packages/riot-route',
@@ -32,6 +33,46 @@ rollup
   .then(bundle => {
     bundle.write({ format: 'es', dest: `${ packageRiotRoute}/index.js` })
     bundle.write({ format: 'cjs', dest: `${ packageRiotRoute }/cjs.route.js` })
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
+rollup
+  .rollup({
+    entry: 'lib/tag.js',
+    external: ['riot'],
+    plugins: [
+      riot(),
+      alias({
+        'riot-observable': 'node_modules/riot-observable/dist/es6.observable.js',
+        'riot-route': 'lib/index.js'
+      }),
+      buble()
+    ]
+  })
+  .then(bundle => {
+    bundle.write({
+      format: 'iife',
+      moduleName: 'route',
+      globals: { riot: 'riot' },
+      dest: `${ packageRiotRouteTag }/route-tag.js`
+    })
+    bundle.write({ format: 'amd', dest: `${ packageRiotRouteTag }/amd.route-tag.js` })
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
+rollup
+  .rollup({
+    entry: 'lib/tag.js',
+    external: ['riot', 'riot-observable', 'riot-route'],
+    plugins: [riot(), buble()]
+  })
+  .then(bundle => {
+    bundle.write({ format: 'es', dest: `${ packageRiotRouteTag }/index.js` })
+    bundle.write({ format: 'cjs', dest: `${ packageRiotRouteTag }/cjs.route-tag.js` })
   })
   .catch(error => {
     console.error(error)
